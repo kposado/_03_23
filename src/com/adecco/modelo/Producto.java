26179bd565cad9c830cc06dc3e6491ee6981df07
kposado <127395149+kposado@users.noreply.github.com>
2023-03-23 13:08:41 +0100

    23 marzo 13:00

---
+++ src/com/adecco/modelo/Producto.java
@@ -0,0 +1,4 @@
+package com.adecco.modelo;
+
+public class Producto {
+}
---
+++ src/com/adecco/presentacion/Main.java
@@ -0,0 +1,11 @@
+package com.adecco.presentacion;
+
+import com.adecco.modelo.Producto;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Producto p1 = new Producto();
+
+    }
+}
